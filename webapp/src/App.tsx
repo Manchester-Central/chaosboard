6aa7575dc8991665352a49d7b9f5a607702232d8
Joshua Allard <jnallard@users.noreply.github.com>
2023-01-12 21:15:22 -0500

    Basic chaos branding (needs improvement)

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 
+interface ntUpdate {
+  key: string;
+  value: any;
+  valueType: string;
+  type: string;
+  id: number;
+  flags: number;
+}
+
+interface ntUpdateMessage {
+  networkTableUpdate: ntUpdate;
+}
+
 function App() {
+  const [entries, setEntries] = useState<Map<string, ntUpdate>>(new Map());
 
   const {sendMessage, getWebSocket} = useWebSocket('ws://localhost:13102', {
     onOpen: (event) => {
       console.log('opened', event);
     },
     onMessage: (event) => {
-      console.log(JSON.parse(event.data));
+      let data: ntUpdateMessage;
+      try {
+        data = JSON.parse(event.data);
+        if(!data.networkTableUpdate) {
+          throw 'socket data does not have networkTableUpdate property';
+        }
+      } catch(error) {
+        console.warn(`can't parse socket data:`, event, error);
+        return;
+      }
+      let update = data.networkTableUpdate;
+      entries.set(update.key, update);
+      setEntries(entries);
     },
     shouldReconnect: (closeEvent) => true,
   });
@@ -18,19 +44,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/ChaosLogo.png" className='App-logo'></img> Board
       </header>
+      <ul>
+        {Array.from(entries.values()).map(entry => <li>{entry.key} {entry.value}</li>)}
+      </ul>
     </div>
   );
 }
