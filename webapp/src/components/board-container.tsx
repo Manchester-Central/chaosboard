0989506c9d156ad195fae48d600f02e18826d559
Joshua Allard <jnallard@users.noreply.github.com>
2023-01-20 21:18:21 -0500

    Better resize and drag support

--- webapp/src/components/board-container.tsx
+++ webapp/src/components/board-container.tsx
@@ -5,106 +5,45 @@ import { NtContextObject } from './nt-container';
 import { onWidgetAdded } from '../components/nt-modal';
 import { useCallback, useEffect, useState } from 'react';
 import useNtEntry from '../hooks/useNtEntry';
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import type { XYCoord } from 'react-dnd';
 import update from 'immutability-helper';
-
-import type { CSSProperties, FC, ReactNode } from 'react'
-
-const boxStyles: CSSProperties = {
-    position: 'absolute',
-    //border: '1px dashed gray',
-    backgroundColor: 'white',
-    //cursor: 'move',
-    //maxWidth: '300px',
-}
-
-const headerStyles: CSSProperties = {
-    cursor: 'move',
-}
-
-export interface BoxProps {
-    id: any
-    left: number
-    top: number
-    zIndex: number
-    hideSourceOnDrag?: boolean
-    children?: ReactNode
-}
-
-export const Box: FC<BoxProps> = ({
-    id,
-    left,
-    top,
-    zIndex,
-    hideSourceOnDrag,
-    children,
-}) => {
-    const [{ isDragging }, drag] = useDrag(
-        () => ({
-            type: 'nt',
-            item: { id, left, top },
-            collect: (monitor) => ({
-                isDragging: monitor.isDragging(),
-            }),
-        }),
-        [id, left, top],
-    )
-
-    if (isDragging && hideSourceOnDrag) {
-        return <div ref={drag} />
-    }
-    return (
-        <div
-            className="card text-center"
-            style={{ ...boxStyles, left, top, zIndex }}
-            data-testid="box"
-        >
-        <div className="card-header text-muted fs-6" ref={drag} style={headerStyles}>
-            <small>{id}</small>
-        </div>
-            <div className="card-body">
-                {children}
-            </div>
-        </div>
-    )
-}
+import { Rnd, DraggableData } from 'react-rnd';
+import { Textfit } from 'react-textfit';
 
 type SimpleDisplayProps = {
     entry: NTEntry | undefined,
 };
 function SimpleDisplay({ entry }: SimpleDisplayProps) {
+    
     let value = useNtEntry(entry);
 
     return (
-        <>{value?.toString()}</>
+        <div style={{width: '100%'}}>
+            
+      <Textfit mode="single">
+      {value?.toString()}
+      </Textfit></div>
     );
-}
-
-const bowWrapperStyles: CSSProperties = {
-    width: '100vw',
-    height: 'calc(100vh - 56px)',
-    border: '1px solid black',
-    position: 'relative',
 }
 
 export interface ContainerState {
     boxes: { [key: string]: { top: number; left: number; title: string } }
 }
 
+interface BoxState {
+    top: number
+    left: number
+    height: string
+    width: string
+    title: string
+    zIndex: number
+}
+
 type BoardContainerProps = {
     manager: NTManager,
-    hideSourceOnDrag: boolean,
 };
-function BoardContainer({ manager, hideSourceOnDrag }: BoardContainerProps) {
+function BoardContainer({ manager }: BoardContainerProps) {
     const [boxes, setBoxes] = useState<{
-        [key: string]: {
-            top: number
-            left: number
-            title: string
-            zIndex: number
-        }
+        [key: string]: BoxState
     }>(JSON.parse(localStorage.getItem('nt-boxes') ?? '{}'))
 
     const getNextZIndex = () => Math.max(...Object.values(boxes).map(box => box.zIndex).filter(z => Number.isSafeInteger(z)), 0) + 1;
@@ -115,8 +54,9 @@ function BoardContainer({ manager, hideSourceOnDrag }: BoardContainerProps) {
                 left: 50,
                 top: 50,
                 title: entry.key,
-                zIndex: getNextZIndex()
-                // TODO: Add high z index so new items are on top
+                zIndex: getNextZIndex(),
+                height: '100px',
+                width: '100px',
             }
             setBoxes(
                 { ...boxes }
@@ -128,67 +68,59 @@ function BoardContainer({ manager, hideSourceOnDrag }: BoardContainerProps) {
         localStorage.setItem('nt-boxes', JSON.stringify(boxes));
     }, [boxes])
 
-    const moveBox = useCallback(
-        (id: string, left: number, top: number) => {
-            const newZIndex = getNextZIndex();
-            console.log(newZIndex, Object.values(boxes).map(box => box.zIndex));
-            setBoxes(
-                update(boxes, {
-                    [id]: {
-                        $merge: { left, top, zIndex: newZIndex },
-                    },
-                }),
-            )
-        },
-        [boxes, setBoxes],
-    )
-    interface DragItem {
-        type: string
-        id: string
-        top: number
-        left: number
-    }
+    const boxDragged = useCallback((event: any, data: DraggableData) => {
+        const newZIndex = getNextZIndex();
+        setBoxes(
+            update(boxes, {
+                [data.node.id]: {
+                    $merge: { left: data.x, top: data.y, zIndex: newZIndex },
+                },
+            }),
+        )
+    }, [boxes, setBoxes]);
 
-    const [, drop] = useDrop(
-        () => ({
-            accept: 'nt',
-            drop(item: DragItem, monitor) {
-                const delta = monitor.getDifferenceFromInitialOffset() as XYCoord
-                const left = Math.round(item.left + delta.x)
-                const top = Math.round(item.top + delta.y)
-                moveBox(item.id, left, top)
-                return undefined
-            },
-        }),
-        [moveBox],
-    )
-    console.log(boxes);
+    const boxResized = useCallback((event: any, direction: any, ref: HTMLElement) => {
+        const newZIndex = getNextZIndex();
+        setBoxes(
+            update(boxes, {
+                [ref.id]: {
+                    $merge: { width: ref.style.width, height: ref.style.height, zIndex: newZIndex },
+                },
+            }),
+        )
+    }, [boxes, setBoxes]);
+
 
     return (
-        <div ref={drop} style={bowWrapperStyles}>
+        <div>
             {Object.keys(boxes).map((key) => {
-                const { left, top, title, zIndex } = boxes[key] as {
-                    top: number
-                    left: number
-                    title: string
-                    zIndex: number
-                }
+                const { left, top, title, zIndex, height, width } = boxes[key]
                 return (
-                    <Box
-                        key={key}
+                    <Rnd
                         id={key}
-                        left={left}
-                        top={top}
-                        zIndex={zIndex}
-                        hideSourceOnDrag={hideSourceOnDrag}
+                        dragHandleClassName='handle'
+                        default={{
+                            x: left,
+                            y: top,
+                            width: width,
+                            height: height,
+                        }}
+                        onDragStop={boxDragged}
+                        onResizeStop={boxResized}
+                        style={{ zIndex }}
                     >
-                        <div>
-                            <SimpleDisplay entry={manager.getEntry(key)}></SimpleDisplay>
+                        <div className='card' style={{ height: '100%' }}>
+                            <div className='handle card-header' style={{ cursor: 'move' }}>
+                                {title}
+                            </div>
+                            <div className='card-body d-flex justify-content-center align-items-center'><SimpleDisplay entry={manager.getEntry(key)}></SimpleDisplay></div>
+
                         </div>
-                    </Box>
+                    </Rnd>
                 )
             })}
         </div>
+
     )
 }
 
@@ -196,10 +128,7 @@ function BoardContainerWrapper() {
 
     return (
         <NtContextObject.Consumer>
-            {context => <DndProvider backend={HTML5Backend}>
-                <BoardContainer manager={context} hideSourceOnDrag={true}></BoardContainer>
-            </DndProvider>}
-
+            {context => <BoardContainer manager={context}></BoardContainer>}
         </NtContextObject.Consumer>
     );
 }
