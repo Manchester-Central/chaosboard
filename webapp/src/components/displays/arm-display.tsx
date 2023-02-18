f8d88487e656c23a4019ad10f86c037ca62c7415
AratosUnderscore <AratosUnderscore@users.noreply.github.com>
2023-02-18 15:27:43 -0500

    Added visuals for arm simulation

--- webapp/src/components/displays/arm-display.tsx
+++ webapp/src/components/displays/arm-display.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { Textfit } from 'react-textfit';
 import { NTEntry } from '../../data/nt-manager';
 import useNtEntry from '../../hooks/useNtEntry';
@@ -9,12 +10,51 @@ export function ArmDisplay({ entry }: ArmDisplayProps) {
 
     let value = useNtEntry(entry);
 
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const draw = (context: CanvasRenderingContext2D, frameCount: number) => {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        console.log(context.canvas.height, context.canvas.width);
+        
+        context.fillStyle = 'hotpink';
+        context.beginPath();
+       // context.arc(170, 50, 30 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+       context.rect(185,200,30,200)
+        context.fill();
+
+        context.strokeStyle = 'green';
+        context.lineWidth = 25
+        context.beginPath();
+        context.moveTo(200,200)
+        context.lineTo(300,200) 
+        context.stroke();
+      };
+      useEffect(() => {
+        const canvas = canvasRef.current
+        if (!canvas) {
+            return;
+       }
+        const context = canvas.getContext('2d');
+        if (!context){
+            return;
+        }
+        let frameCount = 0;
+        let animationFrameId: number;
+        const render = () => {
+          frameCount++;
+          draw(context, frameCount);
+          animationFrameId = window.requestAnimationFrame(render);
+        };
+        render();
+        return () => {
+          window.cancelAnimationFrame(animationFrameId);
+        };
+      }, []);
+
     return (
         <div style={{ width: '100%', textAlign: 'center' }} className={'p-2'}>
-            Arm
-            <Textfit mode="single" max={60}>
-                {value?.toString().replaceAll(',', ', ')}
-            </Textfit>
+            <canvas width="400" height="400" style={{ width: '100%', height: '100%',border:'1px solid black' }} ref={canvasRef}></canvas>
+            
         </div>
     );
+
 }
