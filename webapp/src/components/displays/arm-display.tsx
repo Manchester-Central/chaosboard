75d59c88bbfb5e342fc0e03109962241210505c9
Joshua Allard <jnallard@users.noreply.github.com>
2023-02-24 23:28:16 -0500

    Refactor and make elements more aligned to CAD dimensions

--- webapp/src/components/displays/arm-display.tsx
+++ webapp/src/components/displays/arm-display.tsx
@@ -1,92 +1,155 @@
 import { useEffect, useRef } from 'react';
-import { Textfit } from 'react-textfit';
 import { NTEntry } from '../../data/nt-manager';
 import useNtEntry from '../../hooks/useNtEntry';
 
+const pixelsPerMeter = 400;
+const metersForDisplay = 3;
+const pixelsForDisplay = pixelsPerMeter * metersForDisplay;
+const metersToPixels = (meters: number) =>  Math.floor(meters * pixelsPerMeter);
+const getXCoordinate = (meters: number, widthPixels: number = 0) =>  metersToPixels(meters) + (pixelsForDisplay / 2) - (widthPixels / 2); // Center the x coordinate
+const getYCoordinate = (meters: number, heightPixels: number = 0) =>  pixelsForDisplay - metersToPixels(meters) - heightPixels; // base the y on the bottom of the shape
+
+class Coordinate {
+  constructor(public x: number, public y: number) {
+
+  }
+
+  static fromMeters(xMeters: number, yMeters: number, widthPixels: number = 0, heightPixels: number = 0): Coordinate {
+    return new Coordinate(getXCoordinate(xMeters, widthPixels), getYCoordinate(yMeters, heightPixels));
+  }
+}
+
+interface Shape {
+  widthMeters: number,
+  heightMeters: number
+  xMeters: number,
+  yMeters: number
+  widthPixels: number,
+  heightPixels: number,
+  coordinate: Coordinate,
+  color: string,
+}
+
 type ArmDisplayProps = {
-    entry: NTEntry | undefined,
+  entry: NTEntry | undefined,
 };
 export function ArmDisplay({ entry }: ArmDisplayProps) {
 
-    let value = useNtEntry(entry);
+  let createShape = (widthMeters: number, heightMeters: number, xMeters: number, yMeters: number, color: string) => {
+    const heightPixels = metersToPixels(heightMeters);
+    const widthPixels = metersToPixels(widthMeters);
+    const shape: Shape = {
+      widthMeters,
+      heightMeters,
+      xMeters,
+      yMeters,
+      widthPixels,
+      heightPixels,
+      coordinate: Coordinate.fromMeters(xMeters, yMeters, widthPixels, heightPixels),
+      color,
+    } 
+    return shape;
+  };
 
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-    const draw = (context: CanvasRenderingContext2D, frameCount: number) => {
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-        
-        context.fillStyle = 'hotpink';
-        context.beginPath();
-       context.rect(185,200,30,200);
-        context.fill();
+  let drawRectangle = (context: CanvasRenderingContext2D, shape: Shape) => {
+    context.fillStyle = shape.color;
+    context.beginPath();
+    context.rect(shape.coordinate.x, shape.coordinate.y, shape.widthPixels, shape.heightPixels);
+    context.fill();
+  }
 
-        context.fillStyle = 'black';
-        context.beginPath();
-        context.rect(157, 340, 86.3, 60);
-        context.fill();
+  let drawRoundRectangle = (context: CanvasRenderingContext2D, shape: Shape, radiusPixels: number) => {
+    context.fillStyle = shape.color;
+    context.beginPath();
+    context.roundRect(shape.coordinate.x, shape.coordinate.y, shape.widthPixels, shape.heightPixels, radiusPixels);
+    context.fill();
+  }
 
-        context.strokeStyle = 'blue';
-        context.lineWidth = 25
-        context.beginPath();
-        context.moveTo(200,200)
-        let shoulderAngle = value?.[0] ?? 0;
-        let shoulderAngleRadians = shoulderAngle * Math.PI/180;
-        let extenderLength = value?.[1]*100 ?? 100;
-        let extenderX = 200 + extenderLength * Math.cos(shoulderAngleRadians);
-        let extenderY = 200 - extenderLength * Math.sin(shoulderAngleRadians);
-        context.lineTo(extenderX, extenderY) 
-        context.stroke();
+  let drawLine = (context: CanvasRenderingContext2D, start: Coordinate, angleDegrees: number, lengthMeters: number, color: string, width: number) => {
+    const startXPixel = start.x;
+    const startYPixel = start.y;
+    context.strokeStyle = color;
+    context.lineWidth = width;
+    context.beginPath();
+    context.moveTo(startXPixel, startYPixel);
+    let angleRadians = angleDegrees * Math.PI / 180;
+    let endX = startXPixel + metersToPixels(lengthMeters) * Math.cos(angleRadians);
+    let endY =  startYPixel - metersToPixels(lengthMeters) * Math.sin(angleRadians);
+    context.lineTo(endX, endY);
+    context.stroke();
+    return new Coordinate(endX, endY);
+  }
 
-        let gripperPower = value?.[3] ?? 0;
-        let gripperColor = 'gray'
-        if (gripperPower > 0){
-          gripperColor = '#134122'
-        }
-        if (gripperPower < 0){
-          gripperColor = '#F26722'
-        }
-          
+  let value = useNtEntry(entry);
 
-        context.strokeStyle = gripperColor;
-        context.lineWidth = 25
-        context.beginPath();
-        context.moveTo(extenderX, extenderY)
-        let wristLength = 30
-        let angleWrist = value?.[2] ?? 0;
-        angleWrist += shoulderAngle
-        let angleWristRadians = angleWrist * Math.PI/180;
-        let wristX = extenderX + wristLength * Math.cos(angleWristRadians)
-        let wristY = extenderY - wristLength * Math.sin(angleWristRadians)
-        context.lineTo(wristX , wristY) 
-        context.stroke();
-      };
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const draw = (context: CanvasRenderingContext2D, frameCount: number) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
-      useEffect(() => {
-        const canvas = canvasRef.current
-        if (!canvas) {
-            return;
-       }
-        const context = canvas.getContext('2d');
-        if (!context){
-            return;
-        }
-        let frameCount = 0;
-        let animationFrameId: number;
-        const render = () => {
-          frameCount++;
-          draw(context, frameCount);
-          animationFrameId = window.requestAnimationFrame(render);
-        };
-        render();
-        return () => {
-          window.cancelAnimationFrame(animationFrameId);
-        };
-      }, [value]);
+    const unsafeColor = frameCount % 100 > 50 ? '#aa000055' : '#aa111177';
+    const unsafeZone = createShape(0.4, 0.130, 0, 0.1, unsafeColor);
+    const shoulderPivotDiamterMeters = 0.2;
+    const shoulderPivot = createShape(shoulderPivotDiamterMeters, shoulderPivotDiamterMeters, 0, 1.12, 'lightgray');
+    const mast = createShape(0.051, 1.39, 0, 0.1, 'forestgreen');
+    const wheelDiameterMeters = 0.102;
+    const wheelLeft = createShape(wheelDiameterMeters, wheelDiameterMeters, -0.27, 0.0, 'blue');
+    const wheelRight = createShape(wheelDiameterMeters, wheelDiameterMeters, 0.27, 0.0, 'blue');
+    const bumpers = createShape(0.863, 0.129, 0, 0.047, 'forestgreen');
 
-    return (
-        <div style={{ width: '100%', textAlign: 'center' }} className={'p-2'}>
-            <canvas width="400" height="400" style={{ width: '100%', height: '100%',border:'1px solid black' }} ref={canvasRef}></canvas>
-            {value?.join(", ")}
-        </div>
-    );
+    let shoulderAngleDegrees = value?.[0] ?? 0;
+    let extenderLengthMeters = value?.[1] ?? 1;
+    const extenderStart = Coordinate.fromMeters(0, shoulderPivot.yMeters + (shoulderPivot.heightMeters / 2));
+    const extenderEndpoint = drawLine(context, extenderStart,  shoulderAngleDegrees, extenderLengthMeters, 'orange', 25);
+
+    let gripperPower = value?.[3] ?? 0;
+    let gripperColor = 'gray';
+    if (gripperPower > 0) {
+      gripperColor = '#134122';
+    }
+    if (gripperPower < 0) {
+      gripperColor = '#F26722';
+    }
+
+    let wristAngleDegrees = value?.[2] ?? 0;
+    wristAngleDegrees += shoulderAngleDegrees;
+    let wristLengthMeters = 0.30;
+    const wristEndpoint = drawLine(context, extenderEndpoint, wristAngleDegrees, wristLengthMeters, gripperColor, 25);
+
+    drawRoundRectangle(context, unsafeZone, 10);
+    drawRoundRectangle(context, shoulderPivot, shoulderPivot.heightPixels / 2);
+    drawRectangle(context, mast);
+    drawRoundRectangle(context, wheelLeft, wheelLeft.heightPixels / 2);
+    drawRoundRectangle(context, wheelRight, wheelRight.heightPixels / 2);
+    drawRoundRectangle(context, bumpers, bumpers.heightPixels / 3);
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas) {
+      return;
+    }
+    const context = canvas.getContext('2d');
+    if (!context) {
+      return;
+    }
+    let frameCount = 0;
+    let animationFrameId: number;
+    const render = () => {
+      frameCount++;
+      draw(context, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [value]);
+
+  return (
+    <div style={{ width: '100%', textAlign: 'center' }} className={'p-2'}>
+      <canvas width={pixelsForDisplay} height={pixelsForDisplay} style={{ width: '100%', height: '100%', border: '1px solid black' }} ref={canvasRef}></canvas>
+      {value?.join(", ")}
+    </div>
+  );
 
 }
