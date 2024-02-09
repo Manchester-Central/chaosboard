c11547796da41734d50c4eb90a2997b84d48d7f8
Joshua Allard <jnallard@users.noreply.github.com>
2024-02-09 18:14:19 -0500

    Adds static beams

--- webapp/src/components/displays/robot-display-2024.tsx
+++ webapp/src/components/displays/robot-display-2024.tsx
@@ -5,6 +5,9 @@ import {
   CanvasHelper,
 } from './util/chaos-canvas';
 
+const chaosGreen = '#134122';
+const chaosGreenTransparent = chaosGreen + 'ee';
+
 type ArmDisplayProps = {
   entry: NTEntry | undefined,
 };
@@ -13,7 +16,7 @@ type ArmDisplayProps = {
  * The physical arm was dismantled, so this remains mostly for reference.
  */
 export function RobotDisplay2024({ entry }: ArmDisplayProps) {
-  const [ch] = useState(new CanvasHelper(3));
+  const [ch] = useState(new CanvasHelper(1.5));
 
   const draw = (context: CanvasRenderingContext2D, [_]: (number | undefined)[], frameCount: number) => {
     // wheels
@@ -30,12 +33,24 @@ export function RobotDisplay2024({ entry }: ArmDisplayProps) {
     const basePlate = ch.createShape(basePlateWidth, basePlateheight, 0, basePlateHeightOffGround, 'silver');
     ch.drawRoundRectangle(context, basePlate, basePlate.heightPixels / 3);
 
+    // static lift beam
+    const beamLength = 0.547852; // todo: confirm
+    const beamWidth = 0.050800;
+    const beamAngleDegrees = 80;
+    ch.drawLine(context, ch.getCoordinateFromMeters(-0.096682, 0.071550), beamAngleDegrees, beamLength, chaosGreen, ch.metersToPixels(beamWidth));
+
+    // static lift brace
+    const braceLength = 0.446015;
+    const braceWidth = 0.025400;
+    const braceAngleDegrees = 49.157005;
+    ch.drawLine(context, ch.getCoordinateFromMeters(-0.308817, 0.180586), braceAngleDegrees, braceLength, chaosGreen, ch.metersToPixels(braceWidth));
+
     // bumpers
     const bumbersWidth = 0.990092;
     const bumbersHeight = 0.129032;
     const bumperHeightOffGround = 0.057150;
     const bumpersOffset = 0.539496 - (0.990092 / 2);
-    const bumpers = ch.createShape(bumbersWidth, bumbersHeight, bumpersOffset, bumperHeightOffGround, 'forestgreen');
+    const bumpers = ch.createShape(bumbersWidth, bumbersHeight, bumpersOffset, bumperHeightOffGround, chaosGreenTransparent);
     ch.drawRoundRectangle(context, bumpers, bumpers.heightPixels / 3);
     ch.addTextToShape(context, bumpers, '131', 'white', 50);
   };
