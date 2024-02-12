ca9441a442226faa03f607793cc7844c275b464c
Joshua Allard <jnallard@users.noreply.github.com>
2024-02-11 21:55:29 -0500

    Makes 2024 field image more crisp

--- webapp/src/data/game-specific-data.ts
+++ webapp/src/data/game-specific-data.ts
@@ -22,7 +22,7 @@ class GameData2023 implements IGameSpecificData {
 }
 
 class GameData2024 implements IGameSpecificData {
-    fieldWidthMeters = 16.569627;
+    fieldWidthMeters = 16.512;
     robotWidthMeters = 0.990;
     robotHeightMeters = 0.876;
     fieldImagePath = '/2024/field.png';
